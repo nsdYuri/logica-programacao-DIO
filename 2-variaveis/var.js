6d502ff8cdd6f078ee9ba6e84cf049c2da502659
Yuri <yurichichedom808@gmail.com>
2023-10-05 20:55:54 -0300

    Variaveis

---
+++ 2-variaveis/var.js
@@ -0,0 +1,17 @@
+// produto
+let nomeProduto = "Eurobonds"; //String
+let branch = 86; // Int
+let moeda = "BSB"; // String
+let system = "Sigom"; // String
+let m3 = True; // Boolean
+
+// Exercicio de fixação
+// declarar variaveis para o cadastro de um usuário
+// nome, idade, celular, cpf, endereco
+
+let nome = "Yuri";
+let idade = 19;
+let celular = "11970233795";
+let cpf = "12334567890";
+let endereco = "Av. Juscelino Kubistchek 2050";
+let maiorDeIdade = True;
