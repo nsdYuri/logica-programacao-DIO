7fab8b7030092ca5a0a4250e8883f66104865d60
Yuri <yurichichedom808@gmail.com>
2023-10-05 20:58:17 -0300

    Estruutras Condicionais

---
+++ 4-estruturas/condicionais.js
@@ -0,0 +1,30 @@
+let nome = "Yuri"
+
+// IF
+if (nome == "Yuri"){
+    console.log("Bem vindo " + nome)
+}
+
+// IF ELSE
+if (nome == "Yuri"){
+    console.log("Bem vindo " + nome)
+} else {
+    console.log("Usuário não identificado")
+}
+
+nome = "Derek"
+if (nome == "Yuri"){
+    console.log("Bem vindo " + nome)
+} else {
+    console.log("Usuário não identificado")
+}
+
+
+// ELSE IF
+if (nome == "Yuri"){
+    console.log("Bem vindo Yuri")
+} else if (nome == "Derek"){
+    console.log("Bem vindo Derek")
+} else {
+    console.log("Usuario não encontrado")
+}
