fb34901b2b48ef2e281af7271f1c5baffdd39664
Yuri <yurichichedom808@gmail.com>
2023-10-19 18:18:23 -0300

    Estruturas de Decisão concluida - switch case

---
+++ 4-estruturas/decisao-SwitchCase.js
@@ -0,0 +1,54 @@
+let mes = 11;
+
+switch (mes){
+    case 1:
+    case "primeiro":
+        console.log("Janeiro");
+    break;
+    case 2:
+    case "segundo":
+        console.log("Fevereiro");
+    break;
+    case 3:
+    case "terceiro":    
+        console.log("Março");
+    break;
+    case 4:
+    case "quarto":        
+        console.log("Abril");
+    break;
+    case 5:
+    case "quinto":    
+        console.log("Maio");
+    break;
+    case 6:
+    case "sexto":
+        console.log("Junho");
+    break;
+    case 7:
+    case "sétimo":    
+        console.log("Julho");
+    break;
+    case 8:
+    case "oitavo":
+        console.log("Agosto");
+    break;
+    case 9:
+    case "nono":
+        console.log("Setembro");
+    break;
+    case 10:
+    case "décimo":
+        console.log("Outubro");
+    break;
+    case 11:
+    case "décimo primeiro":
+        console.log("Novembro");
+    break;
+    case 12:
+    case "décimo segundo":
+        console.log("Dezembro");
+    break;
+    default:
+        console.log("Mês invalido!")
+}
