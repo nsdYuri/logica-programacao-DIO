f23dd562f0ccaa8b038c98cf92c099194f8a5260
Yuri <yurichichedom808@gmail.com>
2023-10-25 22:39:00 -0300

    Funções sem parametros finalizado

---
+++ 5-funcoes/funcaoSmParametro.js
@@ -0,0 +1,21 @@
+// Criando função que exibe mensagem de boas vindas
+function bemVindo(){
+    console.log("Seja bem vindo ao nosso sistema ")
+}
+
+
+// Crinado função que exibe opções do sistemma
+function opcoes(){
+    console.log("Escolah uma das opções em nosso catalogo| " + 
+    " 1 - Cadastro| " + 
+    " 2 - Login| " +
+    " 3 - Alterar Senha| " +
+    " 4 - Sair")
+}
+
+
+// Chamando função de boas vindas
+bemVindo()
+
+// Chamando função de opções
+opcoes()
